b47530a6a9cde32be48a7b947aabda28851e7935
AlexyLegrand <alexy.legrand@epitech.eu>
2026-04-22 12:50:12 +0200

    feat(primitives): implement plane shape and ray intersection

--- src/primitives/plane/src/lib.rs
+++ src/primitives/plane/src/lib.rs
@@ -12,10 +12,23 @@ pub struct Plane {
 impl CanHit for Plane {
     fn hit(&self, ray: &Ray) -> Option<HitRecord> {
         let denominator = dot(self.normal, ray.direction);
-        if denominator.abs() < 1e-6 { 
+        if denominator.abs() < 1e-6 { // if denominator equals 0, the ray is perpendicular to the normal
+            return None;              // (therefore parallel to the plane), it does not touch it.
+        }
+        let difference = self.position - ray.position;
+        let t = dot(difference, self.normal) / denominator; // determine the distance to the plane using the camera's radius
+        if t < 0.001 {
             return None;
         }
-        None
+        let point = ray.position + t * ray.direction; // find where the impact lies 
+        let normal = self.normal;
+        Some(HitRecord { t, point, normal})
+    }
+}
+
+impl Plane {
+    pub fn new(position: Vec3, normal: Vec3) -> Self {
+        Plane { position, normal }
     }
 }
 
@@ -28,5 +41,5 @@ struct PlaneConfig {
 #[unsafe(no_mangle)]
 pub fn create(cfg: &ron::Value) -> Primitive {
     let config: PlaneConfig = cfg.clone().into_rust().expect("invalid plane config");
-    Box::new(Plane::new(config.position, config.normal));
+    Box::new(Plane::new(config.position, config.normal))
 }
